7221c01149eaaa7a66daedae3d88baaab80e0629
sven.dupli <sven.dupli@gmail.com>
2016-12-27 07:13:08 -0500

    vanilla single-user system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  # Overriding registrations controller (to set up single user system for devise)
+  devise_for :users, controllers: { registrations: "my_registrations"}
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '/about', to: 'static_pages#about'
